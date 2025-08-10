977b6fcb4f9cbdef70f56cd58396f42e929fb339
Jannik Adam <44022350+JannikAdam97@users.noreply.github.com>
2025-08-10 21:01:00 +0200

    feat: remove Google Fonts and document testing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,21 +2,15 @@ import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function Document() {
-  return (
-    <Html lang="de">
-      <Head>
-        <Script src="https://cloudbuds.de/proxy.js" strategy="lazyOnload" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Azeret+Mono:ital,wght@0,400;0,500;1,400;1,500&family=DM+Mono:ital,wght@0,400;0,500;1,400;1,500&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="de">
+            <Head>
+                <Script src="https://cloudbuds.de/proxy.js" strategy="lazyOnload" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
